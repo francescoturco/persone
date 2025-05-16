5845802f52a72da3211b0ff49e5ba2d3922a6b90
fratur <francescoturco2795@gmail.com>
2025-05-16 18:57:02 +0200

    first commit

---
+++ persone.js
@@ -0,0 +1,14 @@
+console.log('PERSONE')
+
+
+/*
+Esercizio 4 
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un'età.
+Partendo da questo, crea quindi un nuovo array inserendo, per ogni persona, una frase (stringa) con il nome e cognome e l'indicazione se può
+guidare, in base all'età.
+*/
+
+
+
+
